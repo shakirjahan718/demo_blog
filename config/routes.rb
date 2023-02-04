53ab9d36c699b6972c0e4db910e42d711b5dce7d
shakir <shakirjahan718@gmail.com>
2023-02-05 00:01:46 +0500

    integrate devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+  }
+  get '/u/:id', to: 'users#profile', as: 'user'
+
   resources :posts
-  root "pages#home"
   get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  root "pages#home"
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+User.create(email: "shakir@example.com", password: "password", password_confirmation: "password")
+
 10.times do |x|
-  Post.create! title: "Title #{x + 1}", body: "Body of post #{x + 1}"
+  Post.create! title: "Title #{x + 1}", body: "Body of post #{x + 1}", user_id: User.first.id
 end
