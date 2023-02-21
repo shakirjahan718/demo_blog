9b0a758602bf48f9ef41cfe4296a2366499b875d
shakir <shakirjahan718@gmail.com>
2023-02-21 14:09:08 +0500

    creating user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,13 @@ class UsersController < ApplicationController
   
   def profile
     @user.update(views: @user.views + 1)
+
+    @posts = @user.posts.includes(:rich_text_body).order(created_at: :desc)
+    @total_views = 0
+
+    @posts.each do |post|
+      @total_views += post.views
+    end
   end
 
   private
