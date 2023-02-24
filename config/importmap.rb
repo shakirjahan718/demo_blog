fde10a1a150930d0e0abc2d499edf69d325c2e26
shakir <shakirjahan718@gmail.com>
2023-02-24 18:02:46 +0500

    Extend Action Text With An Emoji Popup Picker

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.js"
+pin "picmo", to: "https://ga.jspm.io/npm:picmo@5.7.6/dist/index.js"
+pin "@picmo/popup-picker", to: "https://ga.jspm.io/npm:@picmo/popup-picker@5.7.6/dist/index.js"
